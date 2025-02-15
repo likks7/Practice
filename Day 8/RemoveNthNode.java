4092ac0a74179d976ac02abd75e489e5dd881840
likks7 <likhitashetty1@gmail.com>
2025-02-15 22:23:27 +0530

    Add files via upload

---
+++ Day 8/RemoveNthNode.java
@@ -0,0 +1,58 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class RemoveNthNode {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = dummy;
+        ListNode second = dummy;
+        
+        // Move first n+1 steps ahead
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+        
+        // Move both pointers until first reaches the end
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        
+        // Remove the Nth node
+        second.next = second.next.next;
+        
+        return dummy.next;
+    }
+    
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        int n = 2;
+        
+        System.out.println("Original List:");
+        printList(head);
+        
+        head = removeNthFromEnd(head, n);
+        
+        System.out.println("Updated List:");
+        printList(head);
+    }
+}
